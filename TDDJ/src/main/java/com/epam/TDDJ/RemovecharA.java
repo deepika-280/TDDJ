22096462d56f24a54af233bf1fd91a278d7c97f6
deepika-280 <60295764+deepika-280@users.noreply.github.com>
2020-07-20 14:19:42 +0530

    Update RemovecharA.java

--- TDDJ/src/main/java/com/epam/TDDJ/RemovecharA.java
+++ TDDJ/src/main/java/com/epam/TDDJ/RemovecharA.java
@@ -3,24 +3,24 @@ package com.epam.TDDJ;
 public class RemovecharA {
 
 	public String Remove(String string) {
-		String result="";
+		String r="";
 		string=string.toUpperCase();
 		if(string.charAt(0)=='A' && string.charAt(1)=='A') {
-			result=result+string.substring(2);	
+			r=r+string.substring(2);	
 		}
 		else if(string.charAt(0)=='A') {
-			result=result+string.substring(1);
+			r=r+string.substring(1);
 			
 		}
 		else if(string.charAt(1)=='A') {
-			result=result+string.substring(0,1)+string.substring(2);
+			r=r+string.substring(0,1)+string.substring(2);
 			
 		}
 		else {
-			result=result+string;	
+			r=r+string;	
 		}
 		
-		return result;	
+		return r;	
 	}
 
 }
